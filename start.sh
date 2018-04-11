f6fef9bde8acdf7a9cafb11f2ca4425af2d311bd
tao <zt3862266@gmail.com>
2018-04-11 11:22:04 +0800

    modify start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 echo "start new process ..."
+ps=`ps -ef | grep go-rabbitmq-dispatcher | grep -v grep -c`
+if [ $ps -gt 0 ];then
+    echo "already have $ps process,give up"!
+    exit 0;
+fi
 nohup ./go-rabbitmq-dispatcher -c config/queue.yaml -l log/grd.log -pidfile run/grd.pid >/dev/null 2>&1 &
 
 exit 0
